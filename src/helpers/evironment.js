ac620c8940090f0ac9a2354f3dbe547238691d47
Abigail White <as.white@aol.com>
2021-05-10 18:26:15 -0400

    i hate my life

--- src/helpers/evironment.js
+++ src/helpers/evironment.js
@@ -5,7 +5,7 @@ case 'localhost' || '127.0.0.1':
     APIURL = 'localhost:3000/';
     break;
 case 'queery2-client.herokuapp.com/':
-    APIURL = 'https://queery-server2.herokuapp.com/'
+    APIURL = 'https://queery-server2.herokuapp.com'
 }
 
 export default APIURL;
