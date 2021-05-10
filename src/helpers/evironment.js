bbd94f1168661e18c0e53138334748a577f42ac4
Abigail White <as.white@aol.com>
2021-05-10 19:02:26 -0400

    please god

--- src/helpers/evironment.js
+++ src/helpers/evironment.js
@@ -2,10 +2,10 @@ let APIURL = '';
 
 switch (window.location.hostname) {
 case 'localhost' || '127.0.0.1':
-    APIURL = 'localhost:3000/';
+    APIURL = 'http://localhost:3000';
     break;
-case 'queery2-client.herokuapp.com/':
-    APIURL = 'https://queery-server2.herokuapp.com/'
+case 'queery2-client.herokuapp.com':
+    APIURL = 'https://queery-server2.herokuapp.com'
 }
 
 export default APIURL;
