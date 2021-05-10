7a14a86dd6f3d5928a77697006b550b6317b1fce
Abigail White <as.white@aol.com>
2021-05-10 18:09:11 -0400

    please god let this work

--- src/helpers/evironment.js
+++ src/helpers/evironment.js
@@ -1,4 +1,11 @@
-let APIURL = 'localhost:3000/';
+let APIURL = '';
 
+switch (window.location.hostname) {
+case 'localhost' || '127.0.0.1':
+    APIURL = 'localhost:3000/';
+    break;
+case 'queery2-client.herokuapp.com/':
+    APIURL = 'https://queery-server2.herokuapp.com/'
+}
 
 export default APIURL;
